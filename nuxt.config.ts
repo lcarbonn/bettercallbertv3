e7bf486802e921ba0c51d3045e45d408b7abcdba
lcarbonn <laurent.carbonnaux@gmail.com>
2023-11-11 18:01:43 +0100

    add wpa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  //devtools: { enabled: true },
   modules: [
     '@bootstrap-vue-next/nuxt',
     'unplugin-icons/nuxt',
+    '@vite-pwa/nuxt'
   ],
   css: [
     'bootstrap/dist/css/bootstrap.min.css',
-    '~/assets/css/custom-theme.scss'
-  ],      
+    '~/assets/css/custom-theme.scss',
+  ],
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      name: 'Better Call Bert',
+      short_name: 'bettercallbert',
+      display: 'standalone',
+      description: 'Better Call Bert',
+      lang: 'fr-FR'
+    }
+  },
 
 })

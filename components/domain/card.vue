f4cffaecd0eb70a6b7f434601431b85f35f02927
lcarbonn <laurent.carbonnaux@gmail.com>
2023-11-25 20:07:56 +0100

    draft card component

---
+++ components/domain/card.vue
@@ -0,0 +1,26 @@
+<template>
+    <BCard :img-src="card.img"
+            :img-alt="card.title"
+            class="h-100"
+            bg-variant="secondary"
+            text-variant="white"
+            :header-bg-variant="theme"
+            overlay>
+        <template #header>
+            <small>{{ card.title }}</small>
+        </template>
+    </BCard>
+</template>
+
+<script setup lang="ts">
+    const props = defineProps({
+        card: {
+            type: Object,
+            default: null
+        },
+        theme: {
+            type: String,
+            default: "primary"
+        },
+    })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
 <template>
-  <div>
-    <p>{{ cards }}</p>
-    <p>{{ user }}</p>
-  </div>
+    <BRow cols="1"
+         cols-lg="2"
+         cols-xl="5">
+      <BCol v-if="!cards"><BSpinner label="Loading..."></BSpinner></BCol>
+      <BCol v-else
+            v-for="card in cards"
+            :key="card.id"
+            class="py-3">
+        <nuxt-link :to="'/cards/' + card.id"
+                  class="nodecoLink">
+          <!-- <BSpinner v-if="!card.img"
+                    label="Loading..."></BSpinner> -->
+          <DomainCard
+                      :card="card"
+                      ></DomainCard>
+          <!-- <DomainCard v-else
+                      :card="card"
+                      :theme="getVariantTheme(card.idTheme)"
+                      ></DomainCard> -->
+        </nuxt-link>
+      </BCol>
+    </BRow>
 </template>
 
 <script setup lang="ts">

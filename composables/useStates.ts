bc98d65c563f1402f972bb1c62d1d5f29dfe7252
lcarbonn <laurent.carbonnaux@gmail.com>
2023-11-28 09:21:19 +0100

    cleaning

--- composables/useStates.ts
+++ composables/useStates.ts
@@ -2,4 +2,4 @@ import type { User } from "firebase/auth";
 export const useFirebaseUser = () => useState<User>("firebaseUser");
 export const useSnackBarMessage = () => useState<String>("snackBarMessage");
 export const useCards = () => useState<CardType[]>("cards");
-export const useCardTest = () =>  useState<{id: string, title: string}[]>('cardtest')
+

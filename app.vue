c812520d10a5b604c01da9c1772ae7a77f96877b
lcarbonn <laurent.carbonnaux@gmail.com>
2023-12-01 23:08:58 +0100

    refacto app vue

--- app.vue
+++ app.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <BaseNavBar  @filter-cards="filterCards" @search-cards="searchCards"></BaseNavBar>
-    <BContainer fluid>
-      <NuxtPage />
-    </BContainer>
-    <BaseFooter :appVersion="version"></BaseFooter>
-    <BaseSnackbar></BaseSnackbar>
+  <div class="d-flex flex-column justify-content-between min-vh-100">
+    <div>
+      <BaseNavBar  @filter-cards="filterCards" @search-cards="searchCards"></BaseNavBar>
+      <BContainer fluid>
+        <NuxtPage />
+      </BContainer>
+    </div>
+    <div class="d-flex flex-column justify-content-end">
+      <BaseFooter :appVersion="version"></BaseFooter>
+      <BaseSnackbar></BaseSnackbar>
+    </div>
   </div>
 </template>
 <script setup lang="ts">

7b9f6355895e6620ba7c0f2e6ebbf3c9ffcef086
lcarbonn <laurent.carbonnaux@gmail.com>
2026-01-15 11:12:00 +0100

    Update CardDetail.vue

--- app/components/domain/CardDetail.vue
+++ app/components/domain/CardDetail.vue
@@ -1,13 +1,21 @@
 <template>
-  <UPageCard
-    :title="card.title"
-    :to="card.link"
-    variant="subtle"
-    :class="bgColor"
-    spotlight
-    spotlight-color="primary">
-    <img :src="card.src"/>
-  </UPageCard>
+  <UCarousel
+    ref="carousel"
+    class="mt-2"
+    :items="items"
+    arrows
+    :prev="{ onClick: gotoPrevious }"
+    :next="{ onClick: gotoNext }">
+    <UPageCard
+      :title="card.title"
+      :to="card.link"
+      variant="subtle"
+      :class="bgColor"
+      spotlight
+      spotlight-color="primary">
+      <img :src="card.src"/>
+    </UPageCard>
+  </UCarousel>
 </template>
 
 <script setup lang="ts">
@@ -17,11 +25,23 @@
       card:ICard;
   }>()
 
+  const carousel = useTemplateRef('carousel')
+
+  const items = computed (() => {
+    carousel.value?.emblaApi?.scrollTo(1)
+    return ["", props.card, ""]
+  })
+
   const bgColor = computed (() => {
     return "bg-"+props.card.color
   })
 
-  const nav2card = async (id:number) => {
+  const gotoPrevious = async () => {
+    const id = props.card.id - 1
+    await navigateTo("/card/"+id)
+  }
+  const gotoNext = async () => {
+    const id = props.card.id + 1
     await navigateTo("/card/"+id)
   }
 
